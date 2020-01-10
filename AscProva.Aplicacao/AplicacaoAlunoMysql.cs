1e739081d017783a8b63efb12f9384d9ed0d6a73
thiagommdc <59583830+thiagommdc@users.noreply.github.com>
2020-01-10 14:14:13 -0300

    0

---
+++ AscProva.Aplicacao/AplicacaoAlunoMysql.cs
@@ -0,0 +1,134 @@
+﻿using AscProva.Modelo;
+using AscProva.RepositorioMysql;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace AscProva.Aplicacao
+{
+    public class AplicacaoAlunoMysql
+    {
+
+        private Repositorio BancoDeDados;
+
+
+        public AplicacaoAlunoMysql()
+        {
+            BancoDeDados = new Repositorio();
+        }
+
+        public void Salva(Alunos Aluno)
+        {
+            using (BancoDeDados = new Repositorio())
+            {
+                var SQLExecuta = string.Format(
+                    "Insert into Alunos(Nome, Turma, Media, MediaEspecial, ProvaUm, ProvaDois, ProvaTres, ProvaFinal, ProvaEspecial) values('{0}', '{1}', 0, 0, 0, 0, 0, 0, 0)",
+                    Aluno.Nome, Aluno.Turma
+                    );
+                BancoDeDados.ExecutaComando(SQLExecuta);
+                BancoDeDados.Dispose();
+            }
+        }
+
+
+        public void Atualiza(Alunos Aluno)
+        {
+            using (BancoDeDados = new Repositorio())
+            {
+                var SQLExecuta = string.Format(
+                    "update Alunos set Media='{0}', MediaEspecial='{1}', ProvaUm='{2}', ProvaDois='{3}', ProvaTres='{4}', ProvaFinal='{5}', ProvaEspecial='{6}' where Id='{7}'",
+                    Aluno.Media.ToString().Replace(",", "."),
+                    Aluno.MediaEspecial.ToString().Replace(",", "."),
+                    Aluno.ProvaUm.ToString().Replace(",", "."),
+                    Aluno.ProvaDois.ToString().Replace(",", "."),
+                    Aluno.ProvaTres.ToString().Replace(",", "."),
+                    Aluno.ProvaFinal.ToString().Replace(",", "."),
+                    Aluno.ProvaEspecial.ToString().Replace(",", "."),
+                    Aluno.Id
+                    );
+                BancoDeDados.ExecutaComando(SQLExecuta);
+                BancoDeDados.Dispose();
+            }
+
+        }
+
+        public void Excluir(Alunos Aluno)
+        {
+            using (BancoDeDados = new Repositorio())
+            {
+                var SQLExecuta = string.Format(
+                    "delete from Alunos where Id='{0}';",
+                    Aluno.Id
+                    );
+                BancoDeDados.ExecutaComando(SQLExecuta);
+                BancoDeDados.Dispose();
+            }
+        }
+
+        public IEnumerable<Alunos> Selecionar(Alunos Aluno)
+        {
+
+            string SQLExecuta;
+            string SQLComplemento;
+
+            MySqlDataReader DataReader;
+            BancoDeDados = new Repositorio();
+
+            var AlunoLista = new List<Alunos> { };
+
+
+            if (Aluno.Id > 0)
+            {
+                SQLExecuta = string.Format(
+                   "Select * from Alunos where Id={0}",
+                   Aluno.Id
+                   );
+            }
+            else
+            {
+                SQLExecuta = "Select * from Alunos";
+                SQLComplemento = "";
+
+                if (Aluno.Nome != "") { SQLComplemento += " or Nome like '%" + Aluno.Nome + "%'"; }
+
+                if (SQLComplemento == "")
+                    goto SkipToEnd;
+
+                SQLComplemento = SQLComplemento.Trim();
+                SQLComplemento = SQLComplemento.Remove(0, 2);
+                SQLExecuta = SQLExecuta + " where " + SQLComplemento;
+
+            SkipToEnd:;
+
+                SQLExecuta = SQLExecuta + " ORDER BY MediaEspecial DESC, Media DESC";
+            }
+
+            DataReader = BancoDeDados.ExecutaComRetorno(SQLExecuta);
+
+            while (DataReader.Read())
+            {
+                AlunoLista.Add(new Alunos
+                {
+                    Id = Convert.ToInt32(DataReader["Id"]),
+                    Nome = DataReader["Nome"].ToString(),
+                    Turma = DataReader["Turma"].ToString(),
+                    Media = Math.Round(Convert.ToDecimal(DataReader["Media"]), 1),
+                    MediaEspecial = Math.Round(Convert.ToDecimal(DataReader["MediaEspecial"]), 1),
+                    ProvaUm = Math.Round(Convert.ToDecimal(DataReader["ProvaUm"]), 1),
+                    ProvaDois = Math.Round(Convert.ToDecimal(DataReader["ProvaDois"]), 1),
+                    ProvaTres = Math.Round(Convert.ToDecimal(DataReader["ProvaTres"]), 1),
+                    ProvaFinal = Math.Round(Convert.ToDecimal(DataReader["ProvaFinal"]), 1),
+                    ProvaEspecial = Math.Round(Convert.ToDecimal(DataReader["ProvaEspecial"]), 1),
+
+                });
+            }
+
+            BancoDeDados.Dispose();
+
+            return AlunoLista;
+
+        }
+
+
+    }
+}
---
+++ AscProva/Controllers/AlunoController.cs
@@ -0,0 +1,80 @@
+﻿using AscProva.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AscProva.Aplicacao;
+
+namespace AscProva.Controllers
+{
+    public class AlunoController : Controller
+    {
+
+        AplicacaoAlunoMysql BancoDeDadosMysql;
+
+        //var BancoDeDados = new AplicacaoAluno();
+
+        public AlunoController()
+        {
+            BancoDeDadosMysql = new AplicacaoAlunoMysql();
+        }
+
+        public ActionResult Index()
+        {
+            return View(BancoDeDadosMysql.Selecionar(new Alunos()));
+        }
+        
+        public ActionResult Cadastrar(Alunos Aluno)
+        {
+                        
+            ViewBag.Javascript = "FechaCadastro()";
+
+            BancoDeDadosMysql.Salva(Aluno);
+
+            return PartialView("_ListaAlunos", BancoDeDadosMysql.Selecionar(new Alunos()));
+        }
+
+
+        public ActionResult PreencheEdicao(int Id)
+        {
+            
+            ViewBag.Javascript = "AbreModalEdicao()";
+            ViewBag.MediaUltimo = BancoDeDadosMysql.Selecionar(new Alunos()).OrderByDescending(x => x.Media).ElementAt(4).Media;
+
+            return PartialView("_Editar", BancoDeDadosMysql.Selecionar(new Alunos() { Id = Id }).First());
+
+        }
+
+        public ActionResult Editar(Alunos Aluno)
+        {
+
+            ViewBag.Javascript = "FechaEdicao()";
+
+            BancoDeDadosMysql.Atualiza(Aluno);
+
+            return PartialView("_ListaAlunos", BancoDeDadosMysql.Selecionar(new Alunos()));
+
+        }
+
+        public ActionResult Excluir(int Id)
+        {
+
+            ViewBag.Javascript = "FechaExclusao()";
+
+            BancoDeDadosMysql.Excluir(new Alunos() { Id = Id });
+
+            return PartialView("_ListaAlunos", BancoDeDadosMysql.Selecionar(new Alunos()));
+
+        }
+
+
+        public ActionResult BuscaNome(string Nome)
+        {
+
+            return PartialView("_ListaAlunos", BancoDeDadosMysql.Selecionar(new Alunos() { Nome = Nome }));
+
+        }
+
+    }
+}
